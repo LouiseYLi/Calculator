04370296c50b018ad3c7cba494aa2a8ca24fe28b
Louise Li <louiseli.van@gmail.com>
2023-12-27 20:15:43 -0800

    created event listener to replace inline event handlers for storing numbers

--- script/calculator.js
+++ script/calculator.js
@@ -1,10 +1,19 @@
+const numberButtons = document.querySelectorAll(".numberButton");
 let calculation = '';
-function calculate() {
-    let sum = eval(calculation);
-    console.log(`${sum}`);
-}
 
 function storeValue(value) {
     calculation += value;
     console.log(`${calculation}`);
 }
+
+function calculate() {
+    let sum = eval(calculation);
+    console.log(`${sum}`);
+}
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", (e => {
+        let buttonValue = e.target.value;
+        storeValue(buttonValue);
+    })
+)});
