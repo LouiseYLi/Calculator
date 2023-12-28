f1bf89821b91aea1cac5f82c54eec7b9a7fab012
Louise Li <louiseli.van@gmail.com>
2023-12-28 04:07:29 -0800

    subsequent operators can replace those that were just entered. +comments for readability

--- script/calculator.js
+++ script/calculator.js
@@ -1,6 +1,7 @@
-const numberButtons = document.querySelectorAll(".numberButton");
-const equals = document.getElementById("equals");
+const buttons = document.querySelectorAll(".allButtons");
 const clear = document.getElementById("clear");
+const percent = document.getElementById("percent");
+const equals = document.getElementById("equals");
 
 let calculation = '';
 let resultContainer = document.getElementById("result");
@@ -8,6 +9,38 @@ let resultValue = document.createElement("p");
 
 updateResult(calculation);
 resultContainer.append(resultValue);
+
+// --------------------------------------------- \\
+// Functions to check value states.
+// --------------------------------------------- \\
+
+function isInteger(str) {
+    return !isNaN(str);
+}
+
+function isOperator(char) {
+    const operators = ['+', '-', '/', '*'];
+    return operators.includes(char);
+}
+
+// --------------------------------------------- \\
+// Functions to retrieve values.
+// --------------------------------------------- \\
+
+function getLastOperator() {
+    for (let i = calculation.length - 1; i >= 0; i--) {
+        if (isOperator(calculation.charAt(i))) {
+            return calculation.substring(i, i + 1);
+        } else if (isInteger(calculation.substring(i, i + 1))) {
+            return null;
+        }
+    }
+    return null;
+}
+
+// --------------------------------------------- \\
+// Updates UI for the calculation result.
+// --------------------------------------------- \\
 
 function updateResult(newValue) {
     let parsedValue = String(newValue);
@@ -19,31 +52,76 @@ function updateResult(newValue) {
     return resultValue;
 }
 
-function storeValue(value) {
-    calculation += value;
-    updateResult(calculation);
-    resultContainer.append(resultValue);
-    console.log(`${calculation}`);
+// --------------------------------------------- \\
+// Functions that manage storing calculation values.
+// --------------------------------------------- \\
+
+function replaceOperator(newOperator) {
+    let oldOperator = getLastOperator();
+    let newCalculation = calculation.replace(oldOperator, newOperator);
+    return newCalculation;
 }
 
-numberButtons.forEach(button => {
+function storeValue(value) {
+    let lastOperator = getLastOperator();
+
+    if (lastOperator !== null && !isInteger(value)) {
+        calculation =  replaceOperator(value);
+    } else {
+        calculation += value;
+    }
+    updateResult(calculation);
+    resultContainer.append(resultValue);
+    return console.log(`${calculation}`);
+}
+
+// --------------------------------------------- \\
+// Updates calculation values.
+// --------------------------------------------- \\
+
+function calculate() {
+    let total = eval(calculation);
+    updateResult(total);
+    resultContainer.append(resultValue);
+    calculation = total;
+    console.log(`${total}`);
+    return calculation;
+}
+
+function clearCalculation() {
+    calculation = "";
+    updateResult(calculation);
+    resultContainer.append(resultValue);
+    return calculation;
+}
+
+function getPercent() {
+    calculate();
+    let percentage = calculation / 100;
+    updateResult(percentage);
+    resultContainer.append(resultValue);
+    console.log(`${percentage}`);
+}
+
+// --------------------------------------------- \\
+// Button event listeners.
+// --------------------------------------------- \\
+
+buttons.forEach(button => {
     button.addEventListener("click", (e => {
         let buttonValue = e.target.value;
         storeValue(buttonValue);
     })
 )});
 
-clear.addEventListener("click", (clearCalculation => {
-    calculation = "";
-    updateResult(calculation);
-    resultContainer.append(resultValue);
-    return calculation;
+clear.addEventListener("click", (() => {
+    clearCalculation();
 }));
 
-equals.addEventListener("click", (calculate => {
-    let total = eval(calculation);
-    updateResult(total);
-    resultContainer.append(resultValue);
-    calculation = total;
-    console.log(`${total}`);
+percent.addEventListener("click", (() => {
+    getPercent();
+}));
+
+equals.addEventListener("click", (() => {
+    calculate();
 }));
